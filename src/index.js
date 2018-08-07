f20c917968ec1e7e6798f7bae6e7b8a74bd206e9
Ryan Carniato <ryansolid@gmail.com>
2018-08-07 00:23:31 -0700

    update simpler api babel plugin

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 import { createRuntime } from 'babel-plugin-jsx-dom-expressions'
-import { autorun, isObservable } from 'mobx'
+import { autorun } from 'mobx'
 
-export default createRuntime({
-  wrap(elem, accessor, isAttr, fn) {
-    return autorun(() => {
-      let value = accessor();
-      if (isObservable(value)) {
-        return autorun(() => fn(elem, value.get()));
-      }
-      fn(elem, value);
-    });
-  }
-})
+export default createRuntime({wrap: autorun})
