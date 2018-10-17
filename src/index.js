3f05e2cca3575d4c75ecc10df718bebeca0e5ac9
Ryan Carniato <ryansolid@gmail.com>
2018-10-16 22:52:30 -0700

    add selection handlers

--- src/index.js
+++ src/index.js
@@ -35,3 +35,40 @@ export function cleanup(fn) {
   let ref;
   return (ref = globalContext) != null ? ref.disposables.push(fn) : void 0;
 }
+
+// ***************
+// Selection handlers
+// ***************
+function shallowDiff(a, b) {
+  let sa = new Set(a), sb = new Set(b);
+  return [a.filter(i => !sb.has(i)), (b.filter(i => !sa.has(i)))];
+}
+
+export function selectWhen(obsv, handler) {
+  return list => {
+    let element = null;
+    const dispose = autorun(() => {
+      const model = obsv();
+      if (element) handler(element, false);
+      if (element = model && list.find(el => el.model === model)) handler(element, true);
+    });
+    cleanup(dispose);
+    return list;
+  }
+}
+
+export function selectEach(obsv, handler) {
+  return list => {
+    let elements = [];
+    const dispose = autorun(() => {
+      const models = obsv(),
+        newElements = list.filter(el => models.indexOf(el.model) > -1),
+        [additions, removals] = shallowDiff(newElements, elements);
+      additions.forEach(el => handler(el, true));
+      removals.forEach(el => handler(el, false));
+      elements = newElements;
+    });
+    cleanup(dispose);
+    return list;
+  }
+}
