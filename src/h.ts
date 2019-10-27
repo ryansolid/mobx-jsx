389bd232945d1cdcb99f16be48ad0370ba951d41
Ryan Carniato <ryansolid@gmail.com>
2019-10-26 23:46:52 -0700

    update version

--- src/h.ts
+++ src/h.ts
@@ -1,6 +1,18 @@
 /// <reference path="../node_modules/dom-expressions/runtime.d.ts" />
 import { createHyperScript } from 'hyper-dom-expressions';
-import * as r from './index';
+import {
+  wrap,
+  insert,
+  createComponent,
+  delegateEvents,
+  classList
+} from './index';
 
 export * from './index';
-export const h = createHyperScript(r);
+export const h = createHyperScript({
+  wrap,
+  insert,
+  createComponent,
+  delegateEvents,
+  classList
+});
--- src/html.ts
+++ src/html.ts
@@ -1,6 +1,18 @@
 /// <reference path="../node_modules/dom-expressions/runtime.d.ts" />
 import { createHTML } from 'lit-dom-expressions';
-import * as r from './index';
+import {
+  wrap,
+  insert,
+  createComponent,
+  delegateEvents,
+  classList
+} from './index';
 
 export * from './index';
-export const html = createHTML(r);
+export const html = createHTML({
+  wrap,
+  insert,
+  createComponent,
+  delegateEvents,
+  classList
+});
